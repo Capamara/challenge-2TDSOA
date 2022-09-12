397981f83b535c6aa9acc0a378f58f4656d547ab
xDust <leonardoctk3@gmail.com>
2022-09-11 22:09:17 -0300

    feat: add fonts load

--- beWisely/App.js
+++ beWisely/App.js
@@ -1,13 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, Text } from 'react-native';
 import Profile from './src/pages/Profile/Profile';
-
+import { Schedule } from './src/pages/Schedule';
+import { Inter_500Medium, Inter_700Bold } from '@expo-google-fonts/inter';
+import { Jost_400Regular, Jost_500Medium, Jost_600SemiBold } from '@expo-google-fonts/jost';
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Jost_600SemiBold,
+    Jost_500Medium,
+    Inter_700Bold,
+    Inter_500Medium,
+    Jost_400Regular
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
   return (
-    <SafeAreaView>
-      <StatusBar/>
-      <Profile/>
-    </SafeAreaView>
+    <>
+      <StatusBar />
+      <Schedule />
+    </>
   );
 }
 
